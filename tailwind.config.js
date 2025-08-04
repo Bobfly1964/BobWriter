b2665c83ca0a80eb304b424ed7c2aa0bd5d82046
Bob Vasya Pest <atary66@gmail.com>
2025-08-04 18:40:45 +0500

    edit pass and CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,17 @@
-// tailwind.config.js
+	// tailwind.config.js
 module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,css}",
+    "./components/**/*.{js,ts,jsx,tsx,css}",
+    "./Writer/**/*.{js,ts,jsx,tsx,css}"
+  ],
+  theme: {
+    extend: {
+      /* ... */
+    }
+  },
+ 
+
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}"
