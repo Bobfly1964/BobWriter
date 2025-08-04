1af506e6d04cfe30cdbbf10d6d3cf1d039e52031
Bob Vasya Pest <atary66@gmail.com>
2025-08-04 20:22:35 +0500

    edit CSS and other

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,25 +4,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
       colors: {
-        border: "hsl(var(--border))", // Должно соответствовать CSS переменной
+        border: "hsl(var(--border))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
-        // Добавьте остальные цвета по аналогии
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
+      }
     },
   },
   plugins: [],
