00250a757e7d4d077d31f38591e141c085609e0b
Bob Vasya Pest <atary66@gmail.com>
2025-08-04 20:04:26 +0500

    edit CSS and other

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
-	// tailwind.config.js
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,css}",
-    "./components/**/*.{js,ts,jsx,tsx,css}",
-    "./Writer/**/*.{js,ts,jsx,tsx,css}"
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      /* Здесь можно оставить другие ваши расширения */
-    }
+      colors: {
+        border: "hsl(var(--border))", // ADD THIS
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        // Add other required colors
+      }
+    },
   },
-  plugins: []
-};
+  plugins: [],
+}
